c57bef068980659c036aa49579a5dd26c4639bf1
Richard Gyiko <richard.gyiko@whizystems.com>
2025-04-07 20:12:11 +0200

    Bump version

--- src/json_schema_to_pydantic/__init__.py
+++ src/json_schema_to_pydantic/__init__.py
@@ -6,7 +6,7 @@ from .exceptions import (
 )
 from .model_builder import PydanticModelBuilder
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 
 from typing import Any, Dict, Optional, Type, TypeVar
